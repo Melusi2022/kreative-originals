642c7cfcb26d31e8445e0b361c5d22470ab63a84
Melusi. L. Khumalo <135502931+Melusi2022@users.noreply.github.com>
2025-07-25 09:46:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const navToggle = document.getElementById('nav-toggle');
 const navLinks = document.getElementById('nav-links');
-const darkToggle = document.getElementById('darkModeToggle');
+const darkModeToggle = document.getElementById('darkModeToggle');
 
-// Toggle mobile nav
 navToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
 
-// Toggle dark mode
-darkToggle.addEventListener('click', () => {
+darkModeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
